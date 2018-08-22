b50cd243e052a4f9a081c28569244b0c46f53bf8
Manuel F <m.frankon08@gmail.com>
2018-08-22 01:03:52 -0300

    Initial commit

---
+++ src/Slot.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Slot extends Component {
+    render() {
+        return (
+            <img src="" alt=""/>
+        );
+    }
+}
+
+ export default Slot;
