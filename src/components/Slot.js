825db05f80834704dacdc8cdb3bc4ef3a4e4e667
Manuel F <m.frankon08@gmail.com>
2018-10-31 22:14:23 -0300

    Add mouse hover texture

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -7,10 +7,20 @@ export default class Slot extends Component {
     constructor(props) {
         super(props);
         this.state = { isHovered: false, stock: 0, icon: props.item ? 'inv_misc_questionmark' : '', disabled: props.disabled };
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
     }
 
     componentDidMount() {
 
+    }
+
+    handleMouseOver() {
+        this.setState({isHovered: true});
+    }
+
+    handleMouseLeave() {
+        this.setState({isHovered: false});
     }
 
     render() {
@@ -24,7 +34,7 @@ export default class Slot extends Component {
         if (this.props.item) {
             return (
                 <div className={`slot-${this.props.col}-${this.props.row} unselectable`}>
-                    <div className={slotClass}>
+                    <div onMouseLeave={this.handleMouseLeave} onMouseOver={this.handleMouseOver} className={slotClass}>
                         <a className="parent-size" href={`https://classicdb.ch/?item=${this.props.item.id}`}>
                             <img className="icon-img" src={`/imgs/icons/medium/${this.props.item.icon}.jpg`} alt=""/>
                             <span className="item-count">{this.state.stock}</span>
